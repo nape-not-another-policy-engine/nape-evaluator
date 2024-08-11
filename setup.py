8c85a236b3f917e3fcc5e9f42027d2b3c3dbd990
Bill Bensing <10779425+BillBensing@users.noreply.github.com>
2024-08-11 09:10:37 -0400

    removed dependencies section since there are no depdendencies

--- setup.py
+++ setup.py
@@ -4,9 +4,6 @@ setup(
     name='nape-eval',
     version='1.0.0',
     py_modules=['main'],
-    install_requires=[
-        # Add your dependencies here
-    ],
     entry_points='''
         [console_scripts]
         nape-eval=main:main
